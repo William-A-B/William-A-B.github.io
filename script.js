ff84213d9805ef7a4b8f0f4f653ef3fe2b7c9493
William-A-B <104766801+William-A-B@users.noreply.github.com>
2024-07-08 19:16:28 +0100

    Update gallery with location page and start adding photos

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-
 // document.addEventListener("DOMContentLoaded", function() {
 //     var footer = document.querySelector("footer");
 //     var lastScrollTop = 0;
