efedd888323f1cc603ef7b39669682e52a674e46
William-A-B <104766801+William-A-B@users.noreply.github.com>
2025-08-19 22:48:07 +0100

    add list of wainwrights file.

--- script.js
+++ script.js
@@ -19,18 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// Create the map
-var map = L.map('map').setView([51.505, -0.09], 13); // Replace with your coordinates
+// // Create the map
+// var map = L.map('map').setView([51.505, -0.09], 13); // Replace with your coordinates
 
-// Add a tile layer (this example uses OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+// // Add a tile layer (this example uses OpenStreetMap)
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution: '&copy; OpenStreetMap contributors'
+// }).addTo(map);
 
-// Add a marker
-L.marker([51.505, -0.09]).addTo(map)
-  .bindPopup('Your cool hiking spot!')
-  .openPopup();
+// // Add a marker
+// L.marker([51.505, -0.09]).addTo(map)
+//   .bindPopup('Your cool hiking spot!')
+//   .openPopup();
 
 // document.addEventListener("DOMContentLoaded", function() {
 //     var footer = document.querySelector("footer");
